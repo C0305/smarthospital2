96d2d90822e43f73bf9ea8a7fcd98c4a17eb099c
cobose <ernesto.cobos@tyson.com>
2019-05-28 16:55:47 -0500

    Se añadio el softdelete

--- app/Models/System/documentManagerDocument.php
+++ app/Models/System/documentManagerDocument.php
@@ -3,9 +3,12 @@
 namespace SmartHospital\Models\System;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class documentManagerDocument extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
     	'name',
 	    'size',
